5672d866e116acbd9e22030fa063cffe1dd33ab2
Peeyush Gupta <peeyush654@gmail.com>
2020-02-26 23:34:17 +0530

    background set

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import girlphoto from './Assets/Images/woman-in-blue-striped-flannel-shirt-holding-a-book-indoors-698928.jpg'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
-        
       </header>
+      <div className="backphoto" style={{backgroundImage:`url(${girlphoto})`}}></div>
     </div>
   );
 }
