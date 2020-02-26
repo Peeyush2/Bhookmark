abfb9c55acbaf9ee2bfd000d6c634d23470c1b62
Peeyush Gupta <peeyush654@gmail.com>
2020-02-27 01:08:27 +0530

    card started // not completed

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import './App.css';
+import React,{Component} from 'react';
+import Cards1 from './Card'
+import './CSS/App.css';
 import girlphoto from './Assets/Images/woman-in-blue-striped-flannel-shirt-holding-a-book-indoors-698928.jpg'
 
 function App() {
   return (
     <div className="App" >
-      <header className="App-header">
-      </header>
-      <div className="backphoto" style={{backgroundImage:`url(${girlphoto})`}}></div>
+      <header className="App-header"/>
+      <Cards1/>
+      <div className="backphoto" style={{backgroundImage:`url(${girlphoto})`}}/>
     </div>
   );
 }
