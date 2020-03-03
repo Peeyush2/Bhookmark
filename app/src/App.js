6f55fb46b295a0a0b10b479e92dd5d4cacceb7ae
Peeyush Gupta <peeyush654@gmail.com>
2020-03-03 08:45:40 +0530

    class added

--- app/src/App.js
+++ app/src/App.js
@@ -3,14 +3,22 @@ import Cards1 from './Card'
 import './CSS/App.css';
 import girlphoto from './Assets/Images/woman-in-blue-striped-flannel-shirt-holding-a-book-indoors-698928.jpg'
 
-function App() {
-  return (
-    <div className="App" >
-      <header className="App-header"/>
-      <Cards1/>
-      <div className="backphoto" style={{backgroundImage:`url(${girlphoto})`}}/>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      attribute : null
+    };
+  }
+  render(){
+    return (
+      <div className="App" >
+        <header className="App-header"/>
+        <Cards1/>
+        <div className="backphoto" style={{backgroundImage:`url(${girlphoto})`}}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- app/src/Card.js
+++ app/src/Card.js
@@ -1,32 +1,54 @@
 import React,{Component} from 'react';
-import './CSS/Card.css'
-function Cards1(){
-    var sign = ['email','password'];
-    return(
-        
-      <div className="outerCard">
-          <div className="Card">
-          <div className="logintext">Login</div>
-          <div className="entertext">Enter your details to login</div>
-          <div className="fields">
-            <input className="field" placeholder= "email/mobile"/><br/>
-            <input className="field" type= "password" placeholder="password"/>
-        </div>
-        <div className="forgottext">Forgot Password?</div>
-        <div className="or">
-            OR
-        </div>
-        <div>
-            <button>Login</button>
-        </div>
-        <div>New here? Create free account</div>
-        <div>
-            <a>Help.</a>
-            <a>Privacy.</a>
-            <a>Terms</a>
-        </div>
-        </div>
-    </div>
-    )
+import './CSS/Card.css';
+class Cards1 extends Component{
+    constructor(props){
+        super(props);
+        this.state= {
+            email: '',
+            password: ""
+        }
+        this.updateEmail = this.updateEmail.bind(this);
+    }
+    updateEmail(e){
+        console.log(e);
+        const value = e.target.email;
+        console.log(value)
+        this.setState({
+            email:value
+        })
+    }
+       
+    render(){
+        return(       
+            <div className="outerCard">
+                <div className="Card">
+                <div className="logintext">Login</div>
+                <div className="entertext">Enter your details to login</div>
+                <div className="fields">
+                  <input 
+                  className="field"
+                  placeholder= "email/mobile" 
+                  value = {this.state.email}
+                  onChange={this.updateEmail}/><br/>
+                  <input className="field" type= "password" placeholder="password"/>
+              </div>
+              <div className="forgottext">Forgot Password?</div>
+              <div className="or">
+                  OR
+              </div>
+              <div>
+                  <button>Login</button>
+              </div>
+              <div>New here? Create free account</div>
+              <div>
+                  <a>Help.</a>
+                  <a>Privacy.</a>
+                  <a>Terms</a>
+              </div>
+              </div>
+          </div>
+          )
+    }
+    
 }
 export default Cards1;
